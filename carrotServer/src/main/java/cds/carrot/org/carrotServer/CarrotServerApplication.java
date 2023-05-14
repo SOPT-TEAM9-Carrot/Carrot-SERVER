27c021f2f2c0a584b9a221e5effaac78d6f3df83
유서린 <sprout3082@gmail.com>
2023-05-14 19:01:40 +0900

    [init] : 프로젝트, gitignore 생성

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/CarrotServerApplication.java
@@ -0,0 +1,13 @@
+package cds.carrot.org.carrotServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotServerApplication.class, args);
+	}
+
+}
---
+++ carrotServer/src/test/java/cds/carrot/org/carrotServer/CarrotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cds.carrot.org.carrotServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
