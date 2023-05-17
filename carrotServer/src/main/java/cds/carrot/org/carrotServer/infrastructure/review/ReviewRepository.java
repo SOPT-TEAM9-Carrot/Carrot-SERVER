2dcaf4c5437b7b45485808499806d7b18a086ab2
이빵주 <yungju9982@naver.com>
2023-05-18 04:38:32 +0900

    #11 [delete] : ReviewRepository에 불필요한 메서드 제거

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/infrastructure/review/ReviewRepository.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/infrastructure/review/ReviewRepository.java
@@ -1,11 +1,8 @@
 package cds.carrot.org.carrotServer.infrastructure.review;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ReviewRepository {
 
-    List<ReviewEntity> findAll();
-
-    Optional<ReviewEntity> findById(Long Id);
+    List<ReviewEntity> findByUserId(Long userId);
 }
