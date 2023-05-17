8d049bb301da050aea84fb7317a8612b1ddc884d
이빵주 <yungju9982@naver.com>
2023-05-17 18:25:41 +0900

    #1 [feat] UserEntity 구현

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/infrastructure/user/UserEntity.java
@@ -0,0 +1,35 @@
+package cds.carrot.org.carrotServer.infrastructure.user;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private float degree;
+
+    private UserEntity(String nickname, String image, float degree) {
+        this.nickname = nickname;
+        this.image = image;
+        this.degree = degree;
+    }
+
+    public static UserEntity newInstance(String nickname, String image, float degree) {
+        return new UserEntity(nickname, image, degree);
+    }
+}
