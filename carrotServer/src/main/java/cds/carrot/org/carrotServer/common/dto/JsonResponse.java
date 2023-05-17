66c824bb9743831592e9eeed8bde929b4f694182
유서린 <sprout3082@gmail.com>
2023-05-17 17:40:15 +0900

    [init] : Api Response 세팅

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/common/dto/JsonResponse.java
@@ -0,0 +1,35 @@
+package cds.carrot.org.carrotServer.common.dto;
+
+import cds.carrot.org.carrotServer.exception.ErrorType;
+import cds.carrot.org.carrotServer.exception.SuccessType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class JsonResponse<T> {
+
+    private final int code;
+    private final boolean success;
+    private final String message;
+    private T data;
+
+    public static JsonResponse success(SuccessType success) {
+        return new JsonResponse<>(success.getHttpStatusCode(), true, success.getMessage());
+    }
+
+    public static <T> JsonResponse<T> success(SuccessType success, T data) {
+        return new JsonResponse<T>(success.getHttpStatusCode(), true, success.getMessage(), data);
+    }
+
+    public static JsonResponse error(ErrorType error) {
+        return new JsonResponse<>(error.getHttpStatusCode(), false, error.getMessage());
+    }
+
+    public static JsonResponse error(ErrorType error, String message) {
+        return new JsonResponse<>(error.getHttpStatusCode(), false, message);
+    }
+}
---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/exception/SuccessType.java
@@ -0,0 +1,27 @@
+package cds.carrot.org.carrotServer.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor(access =  AccessLevel.PRIVATE)
+public enum SuccessType {
+
+    /**
+     * 201 CREATED
+     */
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료됐습니다."),
+    CREATE_BOARD_SUCCESS(HttpStatus.CREATED, "게시물 생성이 완료됐습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
