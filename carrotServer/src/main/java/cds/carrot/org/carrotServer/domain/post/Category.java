5bb6d50507972d5a464aeddeffd276ec1c4781dd
유서린 <sprout3082@gmail.com>
2023-05-17 20:46:04 +0900

    [feat] Post, category 도메인 모델 추가

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/domain/post/Category.java
@@ -0,0 +1,12 @@
+package cds.carrot.org.carrotServer.domain.post;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class Category {
+
+    private String name;
+}
