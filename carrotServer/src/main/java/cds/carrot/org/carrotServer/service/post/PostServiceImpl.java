b1746f75afd63f51b0d1da26d6836765286f5fb0
유서린 <sprout3082@gmail.com>
2023-05-17 20:50:14 +0900

    #5 [feat] : Post 비즈니스 로직 구현

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/service/post/PostServiceImpl.java
@@ -0,0 +1,55 @@
+package cds.carrot.org.carrotServer.service.post;
+
+import cds.carrot.org.carrotServer.domain.post.Category;
+import cds.carrot.org.carrotServer.domain.post.Post;
+import cds.carrot.org.carrotServer.infrastructure.category.CategoryEntity;
+import cds.carrot.org.carrotServer.infrastructure.post.PostEntity;
+import cds.carrot.org.carrotServer.infrastructure.post.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+
+    private final PostRepository postRepository;
+
+    @Override
+    public List<Post> getAll() {
+        List<PostEntity> postEntities = postRepository.findAll();
+        return postEntities
+                .stream()
+                .map(this::fromPostEntityToPostMapper)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Post getById(Long postId) {
+        PostEntity postEntity = postRepository.findById(postId)
+                .orElseThrow(() -> new RuntimeException("id에 해당하는 글이 없습니다."));
+        return fromPostEntityToPostMapper(postEntity);
+    }
+
+    private Post fromPostEntityToPostMapper(PostEntity postEntity) {
+        return Post.builder()
+                .company(postEntity.getCompany())
+                .title(postEntity.getTitle())
+                .content(postEntity.getContent())
+                .imageUrl(postEntity.getImageUrl())
+                .address(postEntity.getAddress())
+                .hourlyWage(postEntity.getHourlyWage())
+                .monthlyWage(postEntity.getMonthlyWage())
+                .categories(fromCategoryEntityListToCategoryList(postEntity.getCategories()))
+                .build();
+    }
+
+    private List<Category> fromCategoryEntityListToCategoryList(List<CategoryEntity> categoryEntityList) {
+        return categoryEntityList
+                .stream()
+                .map((categoryEntity -> new Category(categoryEntity.getName())))
+                .collect(Collectors.toList());
+    }
+}
