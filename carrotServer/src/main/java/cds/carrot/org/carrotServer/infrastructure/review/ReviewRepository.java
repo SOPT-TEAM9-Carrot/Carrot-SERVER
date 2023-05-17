4832c4f76ca43f840d6d3bfb3ab55c9088a82e35
이빵주 <yungju9982@naver.com>
2023-05-18 00:07:17 +0900

    #11 [add] ReviewRepository에 find 함수 추가

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/infrastructure/review/ReviewRepository.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/infrastructure/review/ReviewRepository.java
@@ -1,5 +1,11 @@
 package cds.carrot.org.carrotServer.infrastructure.review;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository {
 
+    List<ReviewEntity> findAll();
+
+    Optional<ReviewEntity> findById(Long Id);
 }
