5696dd0cdbdf2d54f933b5f3c60d0762aaa9f54e
유서린 <sprout3082@gmail.com>
2023-05-17 19:10:04 +0900

    [feat] : Post, Category Repository 추가

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/infrastructure/category/CategoryRepositoryImpl.java
@@ -0,0 +1,6 @@
+package cds.carrot.org.carrotServer.infrastructure.category;
+
+import org.springframework.data.repository.Repository;
+
+public interface CategoryRepositoryImpl extends Repository<CategoryEntity, Long>, CategoryRepository{
+}
