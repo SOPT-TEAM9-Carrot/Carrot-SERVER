52d791954b9ed903726d585b660a0f3397ccc1d6
이빵주 <yungju9982@naver.com>
2023-05-19 06:38:19 +0900

    #13 [add] : Review에 Getter 추가

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/domain/employer/Review.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/domain/employer/Review.java
@@ -2,7 +2,9 @@ package cds.carrot.org.carrotServer.domain.employer;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class Review {
