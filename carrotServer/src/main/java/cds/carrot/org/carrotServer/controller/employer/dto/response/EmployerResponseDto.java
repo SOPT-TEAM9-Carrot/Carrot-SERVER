1188c358f82442cfe137d69e20c2acae5d575c06
이빵주 <yungju9982@naver.com>
2023-05-19 06:38:47 +0900

    #13 [feat] : EmployerResponseDto builder 구현

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/controller/employer/dto/response/EmployerResponseDto.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/controller/employer/dto/response/EmployerResponseDto.java
@@ -1,7 +1,9 @@
 package cds.carrot.org.carrotServer.controller.employer.dto.response;
 
 import cds.carrot.org.carrotServer.domain.employer.Review;
+import cds.carrot.org.carrotServer.domain.employer.User;
 import cds.carrot.org.carrotServer.service.employer.EmployerServiceImpl;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @Getter
+@Builder
 @NoArgsConstructor
 public class EmployerResponseDto {
 
@@ -31,5 +34,13 @@ public class EmployerResponseDto {
         this.reviews = reviews;
     }
 
+    public static EmployerResponseDto of(User user, List<Review> reviews) {
+        return EmployerResponseDto.builder()
+                .userId(user.getUserId())
+                .nickname(user.getNickname())
+                .imageUrl(user.getImageUrl())
+                .degree(user.getDegree())
+                .reviews(reviews)
+                .build();
     }
 }
