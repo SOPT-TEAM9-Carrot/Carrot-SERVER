4160277387b83ac2a78f4e93f405a51842c39306
이빵주 <yungju9982@naver.com>
2023-05-19 00:48:53 +0900

    #13 [add] : ControllerExceptionAdvice 추가

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/common/ControllerExceptionAdvice.java
@@ -0,0 +1,48 @@
+package cds.carrot.org.carrotServer.common;
+
+import cds.carrot.org.carrotServer.common.dto.ErrorType;
+import cds.carrot.org.carrotServer.common.dto.JsonResponse;
+import cds.carrot.org.carrotServer.exception.ApiException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+
+@RestControllerAdvice
+public class ControllerExceptionAdvice {
+
+    /**
+     * 400 BAD_REQUEST
+     */
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected JsonResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        FieldError fieldError = (FieldError) Objects.requireNonNull(e.getFieldError());
+        return JsonResponse.error(ErrorType.REQUEST_HEADER_TOKEN_EXCEPTION, String.format("%s. (%s)", fieldError.getDefaultMessage(), fieldError.getField()));
+    }
+
+    /**
+     * 500 Internal Server
+     */
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({Exception.class})
+    protected JsonResponse<Object> handleException(final Exception e) {
+        return JsonResponse.error(ErrorType.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * api custom error
+     */
+
+    @ExceptionHandler({ApiException.class})
+    protected ResponseEntity<JsonResponse> handleApiException(ApiException e) {
+        return ResponseEntity.status(e.getHttpStatus()).body(JsonResponse.error(e.getError(), e.getMessage()));
+    }
+}
