a00a2a0ef2cf3be33f6c56aff9688a2e312a7309
유서린 <sprout3082@gmail.com>
2023-05-18 18:50:12 +0900

    #9 [fix] : 게시물 조회 성공 status code 수정 -> 200

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/common/dto/SuccessType.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/common/dto/SuccessType.java
@@ -13,7 +13,7 @@ public enum SuccessType {
     SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료됐습니다."),
     CREATE_BOARD_SUCCESS(HttpStatus.CREATED, "게시물 생성이 완료됐습니다."),
 
-    FIND_BOARD_SUCCESS(HttpStatus.FOUND, "게시물 조회를 성공했습니다."),
+    FIND_BOARD_SUCCESS(HttpStatus.OK, "게시물 조회를 성공했습니다."),
     ;
 
     private final HttpStatus httpStatus;
