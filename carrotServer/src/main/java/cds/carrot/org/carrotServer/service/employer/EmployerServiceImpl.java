6e01fc0fcd16e1f7770661f159e92c2e9398d05f
이빵주 <yungju9982@naver.com>
2023-05-18 23:06:03 +0900

    #13 [fix] : Employer Service/ServiceImpl 수정

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/service/employer/EmployerServiceImpl.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/service/employer/EmployerServiceImpl.java
@@ -1,13 +1,18 @@
 package cds.carrot.org.carrotServer.service.employer;
 
+import cds.carrot.org.carrotServer.common.dto.ErrorType;
+import cds.carrot.org.carrotServer.controller.employer.dto.response.EmployerResponseDto;
 import cds.carrot.org.carrotServer.domain.employer.Review;
 import cds.carrot.org.carrotServer.domain.employer.User;
+import cds.carrot.org.carrotServer.exception.BadRequestException;
+import cds.carrot.org.carrotServer.exception.NotFoundException;
 import cds.carrot.org.carrotServer.infrastructure.review.ReviewEntity;
 import cds.carrot.org.carrotServer.infrastructure.review.ReviewRepository;
 import cds.carrot.org.carrotServer.infrastructure.user.UserEntity;
 import cds.carrot.org.carrotServer.infrastructure.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,18 +25,30 @@ public class EmployerServiceImpl implements EmployerService {
     private final ReviewRepository reviewRepository;
 
     @Override
-    public User getUserWithReviews(Long userId, int size) {
+    @Transactional
+    public EmployerResponseDto getUserWithReviews(Long userId, int size) {
+        if (size < 0) {
+            throw new BadRequestException(ErrorType.REQUEST_SIZE_EXCEPTION, ErrorType.REQUEST_SIZE_EXCEPTION.getMessage());
+        }
 
         UserEntity userEntity = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("해당 사용자를 찾을 수 없습니다."));
+                .orElseThrow(() -> new NotFoundException(ErrorType.NOT_FOUND_USER_EXCEPTION, ErrorType.NOT_FOUND_USER_EXCEPTION.getMessage()));
 
         List<ReviewEntity> reviewEntities = reviewRepository.findByUserId(userId);
 
         int maxSize = Math.min(size, reviewEntities.size());
 
-        User user = fromUserEntityToUserMapper(userEntity, maxSize);
+        List<Review> reviews = getLimitedReviews(reviewEntities, maxSize);
 
-        return user;
+        User user = fromUserEntityToUserMapper(userEntity);
+
+        return EmployerResponseDto.of(
+                user.getUserId(),
+                user.getNickname(),
+                user.getImageUrl(),
+                user.getDegree(),
+                reviews
+        );
     }
 
     private List<Review> getLimitedReviews(List<ReviewEntity> reviewEntityList, int maxSize) {
@@ -41,16 +58,11 @@ public class EmployerServiceImpl implements EmployerService {
                 .collect(Collectors.toList());
     }
 
-    private User fromUserEntityToUserMapper(UserEntity userEntity, int size) {
-        List<ReviewEntity> reviewEntities = userEntity.getReviewEntities();
-
-        List<Review> reviews = getLimitedReviews(reviewEntities, size);
-
+    private User fromUserEntityToUserMapper(UserEntity userEntity) {
         return User.builder()
                 .nickname(userEntity.getNickname())
                 .imageUrl(userEntity.getImageUrl())
                 .degree(userEntity.getDegree())
-                .reviews(reviews)
                 .build();
     }
 
@@ -59,7 +71,6 @@ public class EmployerServiceImpl implements EmployerService {
                 .reviewerName(reviewEntity.getReviewerName())
                 .imageUrl(reviewEntity.getImageUrl())
                 .comment(reviewEntity.getComment())
-                .userId(reviewEntity.getUser().getId())
                 .build();
     }
 }
