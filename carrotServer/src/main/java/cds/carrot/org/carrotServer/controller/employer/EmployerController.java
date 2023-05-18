43d201fdd1b424e88e7a66c46b3b0fae484e1e8d
이빵주 <yungju9982@naver.com>
2023-05-19 00:49:39 +0900

    #13 [fix] EmployerController 예외처리 수정

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/controller/employer/EmployerController.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/controller/employer/EmployerController.java
@@ -5,7 +5,6 @@ import cds.carrot.org.carrotServer.common.dto.JsonResponse;
 import cds.carrot.org.carrotServer.common.dto.SuccessType;
 import cds.carrot.org.carrotServer.controller.employer.dto.response.EmployerResponseDto;
 import cds.carrot.org.carrotServer.exception.BadRequestException;
-import cds.carrot.org.carrotServer.exception.NotFoundException;
 import cds.carrot.org.carrotServer.service.employer.EmployerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,18 +20,13 @@ public class EmployerController {
     @GetMapping
     @ResponseStatus(HttpStatus.FOUND)
     public JsonResponse<EmployerResponseDto> getReviewList(@RequestHeader String authorization, @PathVariable Long userId, @RequestParam int size) {
-        try {
-            if (!("0".equals(authorization) || "1".equals(authorization))) {
-                throw new BadRequestException(ErrorType.REQUEST_HEADER_TOKEN_EXCEPTION, ErrorType.REQUEST_HEADER_TOKEN_EXCEPTION.getMessage());
-            }
-
-            EmployerResponseDto responseDto = employerService.getUserWithReviews(userId, size);
-            return JsonResponse.success(SuccessType.READ_JOB_LIST_SUCCESS, responseDto);
-        } catch (NotFoundException | BadRequestException e) {
-            return JsonResponse.error(e.getError(), e.getMessage());
-        } catch (Exception e) {
-            return JsonResponse.error(ErrorType.INTERNAL_SERVER_ERROR, ErrorType.INTERNAL_SERVER_ERROR.getMessage());
+        if (!("0".equals(authorization) || "1".equals(authorization))) {
+            throw new BadRequestException(ErrorType.REQUEST_HEADER_TOKEN_EXCEPTION, ErrorType.REQUEST_HEADER_TOKEN_EXCEPTION.getMessage());
         }
+
+        EmployerResponseDto responseDto = employerService.getUserWithReviews(userId, size);
+        return JsonResponse.success(SuccessType.READ_JOB_LIST_SUCCESS, responseDto);
+
     }
 
 }
