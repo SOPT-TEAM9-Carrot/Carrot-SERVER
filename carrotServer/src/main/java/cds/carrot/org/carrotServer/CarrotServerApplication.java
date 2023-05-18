05a8ada32c8a278baeb2e09063ea605a5652dac8
이빵주 <yungju9982@naver.com>
2023-05-19 00:50:20 +0900

    #13 [add] Application에 EnableJpaAudting 어노테이션 추가

--- carrotServer/src/main/java/cds/carrot/org/carrotServer/CarrotServerApplication.java
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/CarrotServerApplication.java
@@ -2,7 +2,9 @@ package cds.carrot.org.carrotServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CarrotServerApplication {
 
