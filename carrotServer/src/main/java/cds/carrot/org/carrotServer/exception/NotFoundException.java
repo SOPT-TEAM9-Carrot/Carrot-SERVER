aaf7b73eedbdf14ef7c08e63d57a62f8773d8264
이빵주 <yungju9982@naver.com>
2023-05-18 23:06:41 +0900

    #13 [add] : exception 추가

---
+++ carrotServer/src/main/java/cds/carrot/org/carrotServer/exception/NotFoundException.java
@@ -0,0 +1,10 @@
+package cds.carrot.org.carrotServer.exception;
+
+import cds.carrot.org.carrotServer.common.dto.ErrorType;
+
+public class NotFoundException extends ApiException {
+
+    public NotFoundException(ErrorType error, String message) {
+        super(error, message);
+    }
+}
